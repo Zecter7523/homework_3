6da6d2b976461f6a58d68b08f72c175e6929fb12
Zecter7523 <zanetomlinson98@gmail.com>
2021-03-18 21:38:28 -0500

    finished funtions and got button to work

--- script.js
+++ script.js
@@ -3,36 +3,37 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-    //Created the arrays
-    let lowerCase =["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"]
-    let upperCase =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Y","Z"]
-    let numbers =[1,2,3,4,5,6,7,8,9,0];
-    let spcChar =["!","@","#","$","%","&","?"]
-    failArray = lowerCase.concat(upperCase,spcChar);
-    //create prompt for character length and confirms for which arrays to concat
-    let passLength = window.prompt("Please enter length of password");
-    //added fail state
-    if(8 > passLength || passLength > 120){
-        console.log("bruh");
-        return;
-    }
-    else if(isNaN(passLength)){
-        console.log("Dude... no");
-        return;
-    }
-    
-    let lCaseP = confirm("add lowercase?");
-    let uCaseP = confirm("add uppercase?");
-    let num = confirm("add numbers?");
-    let spcC = confirm("add special Characters?");
 
-    let selectorArray = [];
+    //Created the arrays
     
     //create the generatepassword function
     function generatePassword(){
+        let lowerCase =["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"]
+        let upperCase =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Y","Z"]
+        let numbers =[1,2,3,4,5,6,7,8,9,0];
+        let spcChar =["!","@","#","$","%","&","?"]
+        failArray = lowerCase.concat(upperCase,spcChar);
+        //create prompt for character length and confirms for which arrays to concat
+        let passLength = window.prompt("Please enter length of password");
+        //added fail state
+        if(8 > passLength || passLength > 120){
+            console.log("bruh");
+            return;
+        }
+        else if(isNaN(passLength)){
+            console.log("Dude... no");
+            return;
+        }
+        
+        let lCaseP = confirm("add lowercase?");
+        let uCaseP = confirm("add uppercase?");
+        let num = confirm("add numbers?");
+        let spcC = confirm("add special Characters?");
+    
+        let selectorArray = [];
+        
         if(lCaseP == true){
-         selectorArray = selectorArray.concat(lowerCase);
+            selectorArray = selectorArray.concat(lowerCase);
         }
         if(uCaseP == true){
          selectorArray =   selectorArray.concat(upperCase);
@@ -45,14 +46,17 @@ function writePassword() {
         }
         console.log(selectorArray);
         //generate the password here
+        let truePWord =" ";
         for(let j = 0; j < passLength; j++){
             let gPassWord = Math.floor(Math.random()*selectorArray.length);
             gPassWord = selectorArray[gPassWord];
-            console.log(gPassWord);
+            truePWord = truePWord.concat(gPassWord);
+            console.log(truePWord);
         
         }
-        return password;
+        return truePWord;
     }
+    function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     
@@ -62,5 +66,5 @@ function writePassword() {
   }
   
   // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword());
+  generateBtn.addEventListener("click", writePassword);
   
