3480fdd5b7d8c4e259e424ac105575dcf4c236b5
Zecter7523 <zanetomlinson98@gmail.com>
2021-03-18 21:04:24 -0500

    bearly finished generate pass word function

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
     //Created the arrays
     let lowerCase =["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"]
-    let upperCase =["A","B","C","D","E","F","G","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"]
+    let upperCase =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Y","Z"]
     let numbers =[1,2,3,4,5,6,7,8,9,0];
     let spcChar =["!","@","#","$","%","&","?"]
     failArray = lowerCase.concat(upperCase,spcChar);
@@ -18,7 +18,7 @@ function writePassword() {
         return;
     }
     else if(isNaN(passLength)){
-        console.log("Dude.. no");
+        console.log("Dude... no");
         return;
     }
     
@@ -27,17 +27,35 @@ function writePassword() {
     let num = confirm("add numbers?");
     let spcC = confirm("add special Characters?");
 
-    
+    let selectorArray = [];
     
     //create the generatepassword function
     function generatePassword(){
-        let selectorArray = [];
-
+        if(lCaseP == true){
+         selectorArray = selectorArray.concat(lowerCase);
+        }
+        if(uCaseP == true){
+         selectorArray =   selectorArray.concat(upperCase);
+        }
+        if(num == true){
+         selectorArray = selectorArray.concat(numbers);
+        }
+        if(spcC == true){
+         selectorArray = selectorArray.concat(spcChar);
+        }
+        console.log(selectorArray);
+        //generate the password here
+        for(let j = 0; j < passLength; j++){
+            let gPassWord = Math.floor(Math.random()*selectorArray.length);
+            gPassWord = selectorArray[gPassWord];
+            console.log(gPassWord);
         
+        }
+        return password;
     }
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
-    console.log("The button has been pressed");
+    
   
     passwordText.value = password;
   
